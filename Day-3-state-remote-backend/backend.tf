d429573e124661c975a27f124b2c80f81325a2ef
Syedmujtaba <syedmujtabahussaini03@gmail.com>
2025-02-17 01:11:33 +0530

    day-3 modfied

--- Day-3-state-remote-backend/backend.tf
+++ Day-3-state-remote-backend/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "syedmujtaba"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-bckend resources then day-5-backend config
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
   }
 }
